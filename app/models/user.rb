2317f65a43c66acde492e1e3f71e60ec712f3c1a
kyamax <kisho.0125@gmail.com>
2022-10-04 21:57:26 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  # PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  # validates_format_of :encrypted_password, with: PASSWORD_REGEX 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :encrypted_password, format: { with: PASSWORD_REGEX, message: '半角英数字混合で入力してください' }
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  validates :nickname, presence: :true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+    
+  validates :birthday, presence: :true
 
 end
