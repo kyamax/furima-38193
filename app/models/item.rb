f968c55e4aae4318c742aa8ab079e39e45e86438
kyamax <kisho.0125@gmail.com>
2022-10-11 21:35:44 +0900

    active_hashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charges
+  belongs_to :prefecture
+  belongs_to :days
+
+  validates :name, :explanation, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_charges_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :days_id, numericality: { other_than: 1, message: "can't be blank" } 
+
 end
