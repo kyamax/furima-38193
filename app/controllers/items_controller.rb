d0fd8d57a36ee06a2e0e0ac017f4575b4d3914da
kyamax <kisho.0125@gmail.com>
2022-10-13 21:44:32 +0900

    商品投稿機能の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,24 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :shipping_charges_id, :prefecture_id, :days_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,13 +5,15 @@ class Item < ApplicationRecord
   belongs_to :shipping_charges
   belongs_to :prefecture
   belongs_to :days
+  has_one_attached :image
 
-  validates :name, :explanation, presence: true
-
+  validates :name, :explanation, :price, presence: true
+  validates :image, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :shipping_charges_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :days_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :price, length: { in: 300..9999999, message: "is out of setting range" },numericality: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
 
 end
