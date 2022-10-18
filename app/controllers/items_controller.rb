78766f57c53aa3d39e0cff9d0eb5f4ae96aa8b6e
kyamax <kisho.0125@gmail.com>
2022-10-18 20:53:25 +0900

    rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :find_item, only: [:show, :edit, :update, :destroy ]
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -50,5 +50,4 @@ class ItemsController < ApplicationController
   def find_item
     @item = Item.find(params[:id])
   end
-
 end
