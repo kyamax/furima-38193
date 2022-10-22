e6bed7ff83954df7caae3636106f49844b0727c2
kyamax <kisho.0125@gmail.com>
2022-10-23 00:08:07 +0900

    モデルの実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
+    validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
+    validates :city
+    validates :address1
+    validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "Input only number"}
+    validates :order_id
+  end
 end
