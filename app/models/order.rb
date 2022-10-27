e16a6f54181e84fb96df58a207d6e86875ac5d30
kyamax <kisho.0125@gmail.com>
2022-10-27 22:13:56 +0900

    Formオブジェクトの実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,8 +4,8 @@ class Order < ApplicationRecord
   belongs_to :item
   has_one :address
 
-  with_options presence: true do
-    validates :user_id
-    validates :item_id
-  end
+  # with_options presence: true do
+  #   validates :user_id
+  #   validates :item_id
+  # end
 end
