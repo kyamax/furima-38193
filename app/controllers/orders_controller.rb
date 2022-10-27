60afd869ac62b0daf7b65eaf5f9994b0781547df
kyamax <kisho.0125@gmail.com>
2022-10-27 22:14:30 +0900

    orderコントローラーのcreateアクションの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,14 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    # @address = Address.new
     @order_form = OrderForm.new
   end
 
   def create
-    binding.pry
+    # binding.pry
     @item = Item.find(params[:item_id])
-    # @address = Address.create(address_params)
     @order_form = OrderForm.new(order_params)
-    # Order.create(order_params)
-    # if @address.valid?
+    # binding.pry
     if @order_form.valid?
       # Payjp.api_key = "sk_test_61eefc9f9d19b2a7903c3705"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
       # Payjp::Charge.create(
@@ -20,7 +17,6 @@ class OrdersController < ApplicationController
       #   currency: 'jpy'                 # 通貨の種類（日本円）
       # )
       @order_form.save
-      # @address.save
       redirect_to root_path
     else
       render 'index'
@@ -29,16 +25,8 @@ class OrdersController < ApplicationController
 
   private
 
-  # def address_params
-  #   params.permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address1, :address2, :phone_number)
-
   def order_params
-    params.permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :address1, :address2, :phone_number)
-    # params.require(:address).permit(:post_code, :prefecture_id, :city, :address1, :address2, :phone_number, :item_id).merge(token: params[:token])
+    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :address1, :address2, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
-
-  # def order_params
-  #   params.permit(:user_id, :item_id)
-  # end
   
 end
